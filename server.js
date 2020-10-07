a1c2299d7423b7b1f7553664a82f4964cb0b70ec
ryan hatfield <hatfield086@gmail.com>
2020-10-07 14:36:26 -0500

    added express app, handler for data parsiing and allow server to listen

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+//---Dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+//---Express App
+var app = express();
+var PORT = 3000;
+
+//---Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//---Server Listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  

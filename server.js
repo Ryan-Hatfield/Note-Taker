ab489cec3d431226f86e15adc88b6deeeb5a7240
ryan hatfield <hatfield086@gmail.com>
2020-10-07 15:07:35 -0500

    added routes to index, notes and db.json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 var app = express();
 var PORT = 3000;
 
-//---Express app to handle data parsing
+//---Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,3 +17,16 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
   
+//--Routes to send user to the appropriate pages.
+//--Index HTML
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+//---Notes HTML
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+//--db.json file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});

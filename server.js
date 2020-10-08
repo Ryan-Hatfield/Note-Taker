79cbb11c2da58c9e2697b0a5148903e893d0cf3b
ryan hatfield <hatfield086@gmail.com>
2020-10-07 21:15:46 -0500

    changed var PORT for heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 //---Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //---Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
